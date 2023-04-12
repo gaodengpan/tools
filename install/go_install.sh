24aa061c720eb26057347c2a68a699c2d6751c05
diejia <diejia@webuy.ai>
2023-04-13 00:26:55 +0800

    init

--- install/go_install.sh
+++ install/go_install.sh
@@ -6,8 +6,8 @@ tar xzf go1.20.3.linux-amd64.tar.gz
 
 mv go /usr/local/
 
-echo "export GOROOT=/usr/local/go
+echo 'export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
-export PATH=$GOROOT/bin:$GOPATH/bin:$PATH" >> ~/.bashrc
+export PATH=$GOROOT/bin:$GOPATH/bin:$PATH' >> ~/.bashrc
 
 source ~/.bashrc
