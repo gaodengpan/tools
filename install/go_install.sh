1ad69afd0c209909348e43685fd1c938ce6799e6
diejia <diejia@webuy.ai>
2023-04-13 22:07:27 +0800

    alias

--- install/go_install.sh
+++ install/go_install.sh
@@ -4,7 +4,7 @@ wget https://go.dev/dl/go1.20.3.linux-amd64.tar.gz
 
 tar xzf go1.20.3.linux-amd64.tar.gz
 
-mv go /usr/local/
+mv go /usr/local/ && rm go1.20.3.linux-amd64.tar.gz
 
 echo 'export GOROOT=/usr/local/go
 export GOPATH=$HOME/go
