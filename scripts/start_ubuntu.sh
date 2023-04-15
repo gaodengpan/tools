cb047a19a06ea4c93053c6bd93881cb3430609a3
diejia <diejia@webuy.ai>
2023-04-16 02:00:17 +0800

    script

--- scripts/start_ubuntu.sh
+++ scripts/start_ubuntu.sh
@@ -4,14 +4,14 @@ exec 2>/dev/null
 CONTAINER_NAME="ubuntu"
 IMAGE=gaodp/ubuntu:latest
 
-version=$(docker version)
-if [ "${#version}" -eq 0 ]; then
+docker version > /dev/null
+if [ $? -ne 0 ]; then
 	echo "docker is not running!"
 	return
 fi
 
 running=$(docker container inspect $CONTAINER_NAME -f {{.State.Running}})
-if [ "${#running}" -eq 0 ]; then
+if [ $? -ne 0 ]; then
 	docker run -it --name $CONTAINER_NAME -v ~/work:/root/work -v ~/data:/root/data $IMAGE
 elif [ "${running}" == "true" ]; then
 	docker exec -it $CONTAINER_NAME /bin/zsh
