71dba97b11d2836cc5d140d2026834fc563012c3
diejia <diejia@webuy.ai>
2023-05-06 12:48:11 +0800

    goproxy

--- install/go_install.sh
+++ install/go_install.sh
@@ -1,6 +1,10 @@
 #! /bin/sh
 
-echo "export GOROOT=$(go env GOROOT)
+echo
+"
+export GO111MODULE=on
+export GOPROXY=https://goproxy.cn
+export GOROOT=$(go env GOROOT)
 export GOPATH=$(go env GOPATH)
 export PATH=$(go env GOROOT)/bin:$(go env GOPATH)/bin:$PATH" >> ~/.zshrc
 
